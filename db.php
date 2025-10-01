7d7b33c9efe4f6014b952d2cbfcca1b51253ef79
sahilrajput0fficial <rajpootsahil51@gmail.com>
2025-10-01 11:40:51 +0530

    Done

--- db.php
+++ db.php
@@ -1,7 +1,13 @@
-<?php 
-$conn = new mysqli("localhost","root","","expense");
+<?php
+$host = "ftpupload.net";      
+$user = "if0_39503733";          
+$pass = "sahilrajput0ffi";          
+$db   = "if0_39503733_expense";  
 
-if(!$conn){
-    die("Sorry not able to connect");
+$mysqli = new mysqli($host, $user, $pass, $db);
+
+// Check connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
 }
 ?>
