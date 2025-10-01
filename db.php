fa013da11786dbcb6a8ff7ee49ca870a6831f0a4
sahilrajput0fficial <rajpootsahil51@gmail.com>
2025-10-01 11:44:39 +0530

    Done

--- db.php
+++ db.php
@@ -1,12 +1,20 @@
 <?php
-$host = "ftpupload.net";      
-$user = "if0_39503733";          
-$pass = "sahilrajput0ffi";          
-$db   = "if0_39503733_expense";  
+
+if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1') {
+    $host = "localhost";
+    $user = "root";
+    $pass = ""; 
+    $db   = "expense"; 
+} else {
+
+    $host = "sql300.infinityfree.com";         
+    $user = "if0_39503733";             
+    $pass = "sahilrajput0ffi";             
+    $db   = "if0_39503733_expense";     
+}
 
 $mysqli = new mysqli($host, $user, $pass, $db);
 
-// Check connection
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
